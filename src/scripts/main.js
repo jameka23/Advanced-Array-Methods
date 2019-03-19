0a9e62c7f4605e3bc82008d9bdc6b0acdbf56c9b
Jameka Echols <jameka.echols@gmail.com>
2019-03-19 15:41:38 -0500

    lightening 3

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,6 +1,6 @@
 console.log(businesses)
 const outEl = document.querySelector("#output")
-outEl.innerHTML = "<h1>Active Businesses</h1>"
+// outEl.innerHTML = "<h1>Active Businesses</h1>"
 
 // businesses.forEach(business => {
 //   outEl.innerHTML += `
@@ -52,20 +52,45 @@ outEl.innerHTML = "<h1>Active Businesses</h1>"
 /*
 Lightning Exercise: Use filter() to create another array named manufacturingBusinesses that will contain all businesses in the manufacturing industry. Display those to the DOM.
 */
-const manufacturingBusinesses = businesses.filter(business => {
-    let inManufacturing = false;
-    if (business.companyIndustry === "Manufacturing") {
-        inManufacturing = true;
+// const manufacturingBusinesses = businesses.filter(business => {
+//     let inManufacturing = false;
+//     if (business.companyIndustry === "Manufacturing") {
+//         inManufacturing = true;
+//     }
+//     return inManufacturing;
+// })
+
+// manufacturingBusinesses.forEach(business => {
+//     outEl.innerHTML += `
+//     <h2>${business.companyName}</h2>
+//     <section>
+//       ${business.addressFullStreet}
+//     </section>
+//   `
+//     outEl.innerHTML += "<hr/>"
+// });
+
+/*Lightning Exercise: Instead of just returning the purchasing agent object, return a new object that has the full name of the purchasing agent, the company name, and the phone number. The data structure is shown below. Use that new data structure to display the agent with their company and phone number */
+
+outEl.innerHTML += "<h1>Purchasing Agents</h1>";
+
+/*
+    Using map(), you extract the purchasing agent object
+    from each business and store it in a new array
+*/
+const agents = businesses.map(business => {
+    return{
+        "fullName": `${business.purchasingAgent.nameFirst} ${business.purchasingAgent.nameLast}`,
+        "company": business.companyName,
+        "phoneNumber": business.phoneWork
     }
-    return inManufacturing;
 })
 
-manufacturingBusinesses.forEach(business => {
-    outEl.innerHTML += `
-    <h2>${business.companyName}</h2>
-    <section>
-      ${business.addressFullStreet}
-    </section>
-  `
-    outEl.innerHTML += "<hr/>" 
+console.table(agents)
+
+agents.forEach(agent => {
+  outEl.innerHTML += `<h1>${agent.fullName}</h1>`;
+  outEl.innerHTML += `<h3>${agent.company}</h3>`;
+  outEl.innerHTML += `<h3>${agent.phoneNumber}</h3>`;
+  outEl.innerHTML += "<hr/>";
 });
